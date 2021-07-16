4c89de79ec2ac038f76779916d777a9a22333739
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-16 16:18:42 -0500

    split and join strings

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,10 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            string sentence = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+            string [] sentenceArr = sentence.Split(".");
+            Console.WriteLine(string.Join(",", sentenceArr));
+
         }
     }
 }
