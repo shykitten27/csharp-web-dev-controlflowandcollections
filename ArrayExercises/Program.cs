235afc4e4a74a50558ca6fe3aaebbc6968d081dc
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-16 16:00:12 -0500

    print only odd numbers in array

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,14 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            int[] ArrOfNums = new int[] { 1, 1, 2, 3, 5, 8 };
+
+            foreach(int i in ArrOfNums){
+                if(i % 2 != 0)
+                {
+                    System.Console.Write("{0} ", i);
+                }
+            }
         }
     }
 }
