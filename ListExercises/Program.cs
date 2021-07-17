d2884b35bd592a3281278c6cf7278d66387132ed
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-16 21:51:47 -0500

    add Split String to List in C#

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ListExercises
 {
     class Program
     {
+        private static object split;
+
         static void Main(string[] args)
         {
             List<double> numbers = new List<double> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             Console.WriteLine("Sum of even numbers: " + SumEven(numbers));
             List<string> words = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
             SearchLetterWords(words);
+            string sentence = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+            List<string> list = new List<string>();
+            list = sentence.Split('.').ToList();
+            foreach (string l in list)
+            {
+                Console.WriteLine(l);
+            }
         }
 
         static double SumEven(List<double> numbers)
