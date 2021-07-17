cd2ab1ca26096afc27d3d7a42948ac2b30194ae3
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-16 19:07:23 -0500

    sum even numbers

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExercises
 {
@@ -6,7 +7,25 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lists!");
+            List<double> numbers = new List<double> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Sum of even numbers: " + sumEven(numbers));
+            List<string> words = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
         }
-    }
+
+        static double sumEven(List<double> numbers)
+        {
+            double sum = 0.0;
+            foreach (double number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                    sum += number;
+                }
+            }
+            return sum;
+        }
+
+        //static string fiveLetterWords(List<string> words)
+    }     
 }
