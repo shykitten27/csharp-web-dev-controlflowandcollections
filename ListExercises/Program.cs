005829080e63989003ed8fe2dc8c478e7b73d835
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-16 20:08:30 -0500

    search words

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -8,11 +8,12 @@ namespace ListExercises
         static void Main(string[] args)
         {
             List<double> numbers = new List<double> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            Console.WriteLine("Sum of even numbers: " + sumEven(numbers));
+            Console.WriteLine("Sum of even numbers: " + SumEven(numbers));
             List<string> words = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+            SearchLetterWords(words);
         }
 
-        static double sumEven(List<double> numbers)
+        static double SumEven(List<double> numbers)
         {
             double sum = 0.0;
             foreach (double number in numbers)
@@ -26,6 +27,18 @@ namespace ListExercises
             return sum;
         }
 
-        //static string fiveLetterWords(List<string> words)
+
+        public static void SearchLetterWords(List<string> words) 
+        {
+            Console.WriteLine("\nEnter the word length for the search: ");
+            string input = Console.ReadLine();
+            double searchLength = double.Parse(input);
+            Console.WriteLine("\nWords containing exactly " + searchLength + " letters:");
+            foreach (string word in words) {
+                if (word.Length == searchLength) {
+                    Console.WriteLine(word);
+                }
+            }
+        } 
     }     
 }
