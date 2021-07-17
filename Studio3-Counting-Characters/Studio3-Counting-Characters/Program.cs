e07ffa02acca34d6dfcb68312cbf4ad6187e31c8
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-16 21:54:53 -0500

    initial commit

---
+++ Studio3-Counting-Characters/Studio3-Counting-Characters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Studio3_Counting_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
